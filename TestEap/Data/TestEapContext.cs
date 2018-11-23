753e0f7d1b6bd8053fed9e3517e3214b8287dee9
daotungsl <daotungsl@gmail.com>
2018-11-23 11:28:40 +0700

    again

---
+++ TestEap/Data/TestEapContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestEap.Controllers;
+
+namespace TestEap.Models
+{
+    public class TestEapContext : DbContext
+    {
+        public TestEapContext (DbContextOptions<TestEapContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestEap.Controllers.Employee> Employee { get; set; }
+    }
+}
---
+++ TestEap/Migrations/20181123032651_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestEap.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(nullable: true),
+                    EmployeeName = table.Column<string>(nullable: true),
+                    Salary = table.Column<string>(nullable: true),
+                    Department = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
